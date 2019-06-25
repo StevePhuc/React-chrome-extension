d7b2b2f0003745e9327d410aa1ad951605665aed
Steve Phuc <stevephuc.dev@gmail.com>
2019-06-25 11:04:19 +0300

    11.4 background scripts

--- public/content.js
+++ public/content.js
@@ -1,7 +1,14 @@
 console.log("Chrome extension to go");
 
-let paragraphs = document.getElementsByTagName("p");
+chrome.runtime.onMessage.addListener(gotMessage);
 
-for (let elt of paragraphs) {
-  elt.style["background-color"] = "#FF00FF";
+function gotMessage(message, sender, sendResponse) {
+  console.log(message.txt);
+  if (message.txt === "hello") {
+    let paragraphs = document.getElementsByTagName("p");
+
+    for (let elt of paragraphs) {
+      elt.style["background-color"] = "#FF00FF";
+    }
+  }
 }
